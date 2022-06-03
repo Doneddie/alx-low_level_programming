7e2903c0a45f054f8b82baa8162293f3d655dfbf
edwardmasembe <edwardmasembe17@gmail.com>
2022-06-03 06:13:01 -0700

    Except q and e the alphabet game

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print all letters except q and e
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
