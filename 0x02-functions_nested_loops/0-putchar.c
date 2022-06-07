91ea9c3f45b5bdfe1e355b092058e8a3b8f356c7
edwardmasembe <edwardmasembe17@gmail.com>
2022-06-07 01:34:32 -0700

    My header file.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Program that prints _putchar.
+ * Return: 0
+ */
+void print (_putchar)
+{
+	// Print _putchar
+	putchar('_putchar');
+	return 0;
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - check code
+ * Description: Print the alphabet, in lowercase.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+
+	}
+	putchar('\n');
+
+}
