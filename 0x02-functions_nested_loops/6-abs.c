38d4d58bb1154193c1e2f801648cd8538b42796f
edwardmasembe <edwardmasembe17@gmail.com>
2022-06-08 00:52:44 -0700

    The absolute test

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - return absolute value
+ * @n : number to check
+ * Return: 0 or 1
+ */
+
+int _abs(int n)
+{
+	return (n >= 0 ? n : n * -1);
+
+}
