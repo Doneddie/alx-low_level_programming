7ca6282a156a5bb8e24d5ce01f849d088b6773f0
edwardmasembe <edwardmasembe17@gmail.com>
2022-06-08 02:49:34 -0700

    learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * times_table - Print 9 times table
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int x = 0;
+	int y;
+	int rep;
+
+	while (x <= 9)
+	{
+		y = 0;
+		while (y <= 9)
+		{
+			rep = x * y;
+			if (y == 0)
+			{
+				_putchar('0' + rep);
+			}
+			else if (rep < 10)
+			{
+				_putchar(' ');
+				_putchar('0' + rep);
+			}
+			else
+			{
+				_putchar('0' + rep / 10);
+				_putchar('0' + rep % 10);
+			}
+
+			if (y < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			y++;
+		}
+		_putchar('\n');
+		x++;
+	}
+}
