9ed0f2f288ad1057b3f6752c364156933adab1b8
edwardmasembe <edwardmasembe17@gmail.com>
2022-06-09 03:17:37 -0700

    The digits are here

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - check for a digit 0 through 9
+ * @c: character to  check
+ * Return:0 or 1
+ */
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+
+}
