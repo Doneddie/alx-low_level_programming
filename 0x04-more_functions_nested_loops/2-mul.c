84989ccfa0cf00d478c545316786a3c38a03fbfb
edwardmasembe <edwardmasembe17@gmail.com>
2022-06-09 03:32:18 -0700

    Its all about multiplying

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - multiplies two intergers
+ * @a: int type number
+ * @b: int type number
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
