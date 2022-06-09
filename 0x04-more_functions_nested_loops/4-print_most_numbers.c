c0502ec536664b22899b2307ba34ed40928fa0c7
edwardmasembe <edwardmasembe17@gmail.com>
2022-06-09 04:12:42 -0700

    I believe in numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints numbers from 0 to 9
+ * no 2 and 4
+ * Return:0
+ */
+void print_most_numbers(void)
+{
+	char n = 0;
+
+	while (n <= 9)
+	{
+		if (n != 2 && n != 4)
+		{
+			_putchar('0' + n);
+		}
+
+		n++;
+	}
+	_putchar('\n');
+
+}
+
