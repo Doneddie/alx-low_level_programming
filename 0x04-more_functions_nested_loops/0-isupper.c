0a299e2ed5de0a213b1c629e2ec891a33318fe0a
edwardmasembe <edwardmasembe17@gmail.com>
2022-06-09 03:04:55 -0700

    Upper cases only

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - checks upppercase characters.
+ * @c: character to check case.
+ * Return:0 or 1
+ */
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+
+}
