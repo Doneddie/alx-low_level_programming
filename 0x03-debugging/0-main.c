24056aad32ea52e9ff0a155a23fecd3a666cacd0
edwardmasembe <edwardmasembe17@gmail.com>
2022-06-10 04:48:44 -0700

    Like, comment, subscribe

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * positive_or_negative - tests for positive or negative
+ * Return: void
+ */
+void positive_or_negative(int i)
+{
+
+	i = 0;
+	positive_or_negative(i);
+
+}
