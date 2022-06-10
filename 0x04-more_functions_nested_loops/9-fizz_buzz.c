17460f8c08c7140784ed8e00f033bfe02a334acb
edwardmasembe <edwardmasembe17@gmail.com>
2022-06-10 01:43:21 -0700

    Fizz buzzers

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,17 +12,17 @@ int main(void)
 
 	while (i <= 100)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("Fizz");
+			printf("FizzBuzz");
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz");
 		}
-		else if (i % 3 == 0 && i % 5 == 0)
+		else if (i % 3 == 0)
 		{
-			printf("FizzBuzz");
+			printf("Fizz");
 		}
 		else
 		{
