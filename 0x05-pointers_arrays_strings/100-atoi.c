5f09d0d12d73e900c5d9f03fba0073cebccfea13
edwardmasembe <edwardmasembe17@gmail.com>
2022-06-14 04:37:29 -0700

    Greatness of leaders who sacrifice numbers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,26 +3,34 @@
 /**
  * _atoi - convert a string to an integer
  * @s: char type string
- * @i: integer 
  * Return: integer converted
  */
 int _atoi(char *s)
 {
-	int sign = 1, resp = 0, firstNum;
+	int i;
+	int h, p;
 
-	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
+	h = 0;
+	p = -1;
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[firstNum] == '-')
+		if (s[i] == '-')
+			p *= -1;
+
+		if (s[i] > 47 && s[i] < 58)
 		{
-			sign *= -1;
+		if (h < 0)
+			h = (h * 10) - (s[i] - '0');
+		else
+			h = (s[i] - '0') * -1;
+
+		if (s[i + 1] < 48 || s[i + 1] > 57)
+			break;
+
 		}
 	}
+	if (p < 0)
+		h *= -1;
 
-	while (int i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
-	{
-		resp *= 10;
-		resp += (s[i] - 48);
-	}
-
-	return (sign * resp);
+	return (0);
 }
