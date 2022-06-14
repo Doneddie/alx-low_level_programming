dd60d2a4813b5e656dd96e34c899b4d3daa3f0d2
edwardmasembe <edwardmasembe17@gmail.com>
2022-06-14 01:18:13 -0700

    Dont sawp horses in a crossing stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - swap values of two int
+ * @a: pointer 1
+ * @b: pointer 2
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	*a += *b;
+	*b = *a - *b;
+	*a = *a - *b;
+}
