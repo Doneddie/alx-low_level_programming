b0ca0676b460feb09bccfa08bf08f9d04599a529
edwardmasembe <edwardmasembe17@gmail.com>
2022-06-14 02:10:15 -0700

    Think in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - prints reversed string
+ * @s: pointer to the string to print
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int len, i, half;
+	char temp;
+
+	for (len = 0; s[len] != '\0'; len++)
+		;
+
+	i = 0;
+
+	half = len / 2;
+
+	while (half--)
+	{
+		temp = s[len - i - 1];
+		s[len - i - 1] = s[i];
+		s[i] = temp;
+		i++;
+	}
+
+}
