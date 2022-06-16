912d34d7b067a94662e0675f60372bd36185756c
edwardmasembe <edwardmasembe17@gmail.com>
2022-06-16 01:47:38 -0700

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * *string_toupper - capitalise lowercase string
+ * @str: pointer
+ * Return: Capitalized string
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] <=  'z' && str[i] >= 'a')
+			str[i] -= 32;
+	}
+	return (str);
+}
