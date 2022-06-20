cbb547198d325080f3af4c5e81b6a6a7aef98fe8
edwardmasembe <edwardmasembe17@gmail.com>
2022-06-20 01:35:16 -0700

    Strspn

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@ int _puthcar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
-unsigned int _strpn(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
