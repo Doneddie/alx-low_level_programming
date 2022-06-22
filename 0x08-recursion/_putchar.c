d689d5b3e3576d2ac81079809cd08de3d1fa6eb4
edwardmasembe <edwardmasembe17@gmail.com>
2022-06-22 12:49:04 -0700

    Recursions mandatory

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - print chacter c
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
