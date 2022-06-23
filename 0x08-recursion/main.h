ba7c0de4cee05b9e847999ccd4bcf5dbcf38051a
edwardmasembe <edwardmasembe17@gmail.com>
2022-06-23 01:12:37 -0700

    Advanced recursions

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,5 +8,8 @@ int square(int n, int val);
 int _sqrt_recursion(int n);
 int check_prime(int n, int resp);
 int is_prime_number(int n);
+int last_index(char *s);
+int check(char *s, int start, int end, int pair);
 int is_palindrome(char *s);
+int checker(char *s1, char *s2, int a, int b, int wildUsed);
 int wildcmp(char *s1, char *s2 );
